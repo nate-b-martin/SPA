c58af0a98f5e9c3782b034241b153eb3f3c4219c
nate-b-martin <nate.martinb@gmail.com>
2025-05-21 09:21:09 -0500

    keep up to date before vibe coding

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,16 @@
+import Intro from "@/components/intro";
+import RecentPosts from "@/components/recent-posts";
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+
+export default function Home() {
+  
+  return (
+      <section className="py-24">
+        <div className="container max-w-3xl">
+          <Intro/>
+          <RecentPosts/>
+        </div>
+      </section>
+  );
+}
