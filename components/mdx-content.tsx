6d0fbf6c7de3169d1aa1fb0216123d37a7e5a7d8
nate-b-martin <nate.martinb@gmail.com>
2026-04-08 11:28:46 -0500

    Fix: formatted table on resume post for technical skill section

--- components/mdx-content.tsx
+++ components/mdx-content.tsx
@@ -1,24 +1,34 @@
-import { JSX } from "react"
+import { JSX } from 'react'
 import { highlight } from 'sugar-high'
-import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc"
-import Counter from "@/components/counter"
+import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc'
+import remarkGfm from 'remark-gfm'
+import Counter from '@/components/counter'
 
 function Code(props: React.HTMLAttributes<HTMLElement>) {
-  const { children, ...restProps } = props
-  const codeHTML = highlight(String(children))
-  return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...restProps} />
+    const { children, ...restProps } = props
+    const codeHTML = highlight(String(children))
+    return (
+        <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...restProps} />
+    )
 }
 
 const components = {
-  code: Code,
-  Counter
+    code: Code,
+    Counter
 }
 
-export default function MDXContent(props: JSX.IntrinsicAttributes & MDXRemoteProps) {
-  return (
-    <MDXRemote 
-      {...props} 
-      components={{ ...components, ...(props.components || {}) }} 
-    />
-  )
+export default function MDXContent(
+    props: JSX.IntrinsicAttributes & MDXRemoteProps
+) {
+    return (
+        <MDXRemote
+            {...props}
+            options={{
+                mdxOptions: {
+                    remarkPlugins: [remarkGfm]
+                }
+            }}
+            components={{ ...components, ...(props.components || {}) }}
+        />
+    )
 }
--- lib/posts.ts
+++ lib/posts.ts
@@ -5,17 +5,17 @@ import matter from 'gray-matter'
 const rootDirectory = path.join(process.cwd(), 'content', 'posts')
 
 export type Post = {
-  metadata: PostMetadata
-  content: string
+    metadata: PostMetadata
+    content: string
 }
 
 export type PostMetadata = {
-  title?: string
-  summary?: string
-  image?: string
-  author?: string
-  publishedAt?: string
-  slug: string
+    title?: string
+    summary?: string
+    image?: string
+    author?: string
+    publishedAt?: string
+    slug: string
 }
 
 /**
@@ -24,14 +24,14 @@ export type PostMetadata = {
  * @returns A promise that resolves to the post object, or null if not found.
  */
 export async function getPostBySlug(slug: string): Promise<Post | null> {
-  try {
-    const filePath = path.join(rootDirectory, `${slug}.mdx`)
-    const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' })
-    const { data, content } = matter(fileContent)
-    return { metadata: { ...data, slug }, content }
-  } catch {
-    return null
-  }
+    try {
+        const filePath = path.join(rootDirectory, `${slug}.mdx`)
+        const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' })
+        const { data, content } = matter(fileContent)
+        return { metadata: { ...data, slug }, content }
+    } catch {
+        return null
+    }
 }
 
 /**
@@ -40,23 +40,23 @@ export async function getPostBySlug(slug: string): Promise<Post | null> {
  * @returns A promise that resolves to an array of post metadata.
  */
 export async function getPosts(limit?: number): Promise<PostMetadata[]> {
-  const files = fs.readdirSync(rootDirectory)
+    const files = fs.readdirSync(rootDirectory)
 
-  const posts = files
-    .map(file => getPostMetadata(file))
-    .sort((a, b) => {
-      if (new Date(a.publishedAt ?? '') < new Date(b.publishedAt ?? '')) {
-        return 1
-      } else {
-        return -1
-      }
-    })
+    const posts = files
+        .map(file => getPostMetadata(file))
+        .sort((a, b) => {
+            if (new Date(a.publishedAt ?? '') < new Date(b.publishedAt ?? '')) {
+                return 1
+            } else {
+                return -1
+            }
+        })
 
-  if (limit) {
-    return posts.slice(0, limit)
-  }
+    if (limit) {
+        return posts.slice(0, limit)
+    }
 
-  return posts
+    return posts
 }
 
 /**
@@ -65,9 +65,9 @@ export async function getPosts(limit?: number): Promise<PostMetadata[]> {
  * @returns The post metadata.
  */
 export function getPostMetadata(filepath: string): PostMetadata {
-  const slug = filepath.replace(/\.mdx$/, '')
-  const filePath = path.join(rootDirectory, filepath)
-  const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' })
-  const { data } = matter(fileContent)
-  return { ...data, slug }
+    const slug = filepath.replace(/\.mdx$/, '')
+    const filePath = path.join(rootDirectory, filepath)
+    const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' })
+    const { data } = matter(fileContent)
+    return { ...data, slug }
 }
